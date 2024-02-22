0402f1900fbc487fa8589aa3e16876fb1c0f3ecd
Sahil <sahilbodke10@gmail.com>
2024-02-23 00:26:11 +0530

    add rj event

--- src/pages/Clubs/GalleryComponent/GalleryItem/GalleryItems.js
+++ src/pages/Clubs/GalleryComponent/GalleryItem/GalleryItems.js
@@ -1,5 +1,12 @@
 export const GalleryItems = [
   {
+    id: 'Inauguration',
+    short: 'Inauguration',
+    description: 'GDSC MGMCET Inauguration',
+    thumbnail: 'or2.JPG',
+    images: ['or3.JPG', 'or2.JPG', 'or1.JPG', 'or4.JPG'],
+  },
+  {
     id: 'Google Cloud',
     short: 'GCSJ 2023',
     description: 'GCSJ 2023',
@@ -7,10 +14,10 @@ export const GalleryItems = [
     images: ['gcsj01.JPG', 'gcsj02.jpg', 'gcsj03.jpg'],
   },
   {
-    id: 'Inauguration',
-    short: 'Inauguration',
-    description: 'GDSC MGMCET Inauguration',
-    thumbnail: 'or2.JPG',
-    images: ['or3.JPG', 'or2.JPG', 'or1.JPG', 'or4.JPG'],
+    id: 'Breaking into Remote Jobs',
+    short: 'Remote Jobs',
+    description: 'Remote Jobs',
+    thumbnail: 'rj-4.JPG',
+    images: ['rj-1.JPG', 'rj-2.jpg', 'rj-3.jpg'],
   }
 ];
