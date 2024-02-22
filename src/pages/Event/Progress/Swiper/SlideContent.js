59ee5dc2d225d42eee8b0c1bb33c436123fe84a3
Sahil <sahilbodke10@gmail.com>
2024-02-22 20:01:50 +0530

    interchange position of orientation with GCSJ

--- src/pages/Event/Progress/Swiper/SlideContent.js
+++ src/pages/Event/Progress/Swiper/SlideContent.js
@@ -1,12 +1,5 @@
 export const SlideContents = [
-  {
-    name: 'Coud Study Jams 2023-24',
-    eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-google-cloud-study-jams-1-info/'],
-    logo: 'cloud-jam-2023-24.jpg',
-    description:
-      'Google Developer Student Clubs (GDSC) are university based community groups for students interested in Google developer technologies. With clubs hosted in 110+ countries around the world, students from undergraduate and graduate programs with an interest in leading a community are welcome.',
-    active: true,
-  },
+
   {
     name: 'GDSC Orientation 2023-24',
     eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-gdsc-orientation-program-2023-2024/'],
@@ -14,6 +7,14 @@ export const SlideContents = [
     description:
       ' Unveiling the GDSC Experience: Learn about GDSCs role in shaping tech enthusiasts into industry-ready developers How to Join: Get a step-by-step guide on becoming a proud member of GDSC Batch 2023. Whether youre a coding maestro or just stepping into the tech world, this session is tailored to empower and inspire you.',
     active: false,
+  },
+  {
+    name: 'Cloud Study Jams 2023-24',
+    eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-google-cloud-study-jams-1-info/'],
+    logo: 'cloud-jam-2023-24.jpg',
+    description:
+      'Google Developer Student Clubs (GDSC) are university based community groups for students interested in Google developer technologies. With clubs hosted in 110+ countries around the world, students from undergraduate and graduate programs with an interest in leading a community are welcome.',
+    active: true,
   },
   {
     name: 'Comming Soon...',
