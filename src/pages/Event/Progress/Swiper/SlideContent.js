a0ea1b863b1687a85ad33b0ad79bef8e93e5580b
Sahil <sahilbodke10@gmail.com>
2024-02-22 22:55:45 +0530

    add 2 events card

--- src/pages/Event/Progress/Swiper/SlideContent.js
+++ src/pages/Event/Progress/Swiper/SlideContent.js
@@ -6,26 +6,34 @@ export const SlideContents = [
     logo: 'Orientation.webp',
     description:
       ' Unveiling the GDSC Experience: Learn about GDSCs role in shaping tech enthusiasts into industry-ready developers How to Join: Get a step-by-step guide on becoming a proud member of GDSC Batch 2023. Whether youre a coding maestro or just stepping into the tech world, this session is tailored to empower and inspire you.',
-    active: false,
+    active: true,
   },
   {
     name: 'Cloud Study Jams 2023-24',
     eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-google-cloud-study-jams-1-info/'],
     logo: 'cloud-jam-2023-24.jpg',
     description:
-      'Google Developer Student Clubs (GDSC) are university based community groups for students interested in Google developer technologies. With clubs hosted in 110+ countries around the world, students from undergraduate and graduate programs with an interest in leading a community are welcome.',
-    active: true,
-  },
-  {
-    name: 'Comming Soon...',
-    eventURL: ['https://gdsc.community.dev/mgms-college-of-engineering-and-technology-navi-mumbai/'],
-    logo: 'comming-soon.png',
-    description:
-      'Stay tuned',
+      'In the dynamic month of September, our club hosted a Google Cloud Study Jams event, where 80+ students successfully completed the Google Cloud Computing Foundations and Generative AI pathways, achieving Tier 1 status.The event fostered collaboration and hands-on learning, creating a vibrant community. ',
     active: false,
   },
   {
-    name: 'Comming Soon...',
+    name: 'Breaking into Remote Jobs',
+    eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-breaking-into-remote-jobs/'],
+    logo: 'remote-jobs2.png',
+    description:
+      'In October 2023, our seminar on remote jobs illuminated new horizons in the evolving work landscape. Distinguished guest speakers Mr.Numan Shaikh and Mr.Parvez Shaikh shared invaluable insights, drawing from their rich expertise. The event explored the nuances of remote work, addressing challenges and providing strategies for success. Attendees gained a deeper understanding of the remote work ecosystem and emerging trends.',
+    active: false,
+  },
+  {
+    name: 'Solution Challenge Info Session',
+    eventURL: ['https://gdsc.community.dev/events/details/developer-student-clubs-mgms-college-of-engineering-and-technology-navi-mumbai-presents-solution-challenge-discover-design-develop-intro-session/'],
+    logo: 'solution-challenge.png',
+    description:
+      'GDSC MGMCET hosted an illuminating Solution Challenge Info Session, guiding students through the intricacies of the upcoming challenge. The session expertly elucidated the challenges nature, encouraging participants to harness their problem-solving prowess. Attendees gained profound insights into the competitions objectives and potential impact, fostering an environment of innovation and collaboration.',
+    active: false,
+  },
+  {
+    name: 'Coming Soon...',
     eventURL: ['https://gdsc.community.dev/mgms-college-of-engineering-and-technology-navi-mumbai/'],
     logo: 'comming-soon.png',
     description:
